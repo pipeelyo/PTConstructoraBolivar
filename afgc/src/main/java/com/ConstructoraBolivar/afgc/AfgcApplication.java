70f251d04682870be7562b27828890b10b874931
Andres Garcia <31287790+pipeelyo@users.noreply.github.com>
2024-07-17 22:59:04 -0500

    Init commit

---
+++ afgc/src/main/java/com/ConstructoraBolivar/afgc/AfgcApplication.java
@@ -0,0 +1,13 @@
+package com.ConstructoraBolivar.afgc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfgcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfgcApplication.class, args);
+	}
+
+}
---
+++ afgc/src/test/java/com/ConstructoraBolivar/afgc/AfgcApplicationTests.java
@@ -0,0 +1,7 @@
+package com.ConstructoraBolivar.afgc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfgcApplicationTests {}
